3914ef84832b3975f22c5c72536375c517331f9d
Alvin Wong <wong.alvin13@gmail.com>
2021-02-25 16:05:31 -0600

    index updated

--- src/index.js
+++ src/index.js
@@ -7,16 +7,63 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
 
-const pizzaReducer = (state = [], action) => {
-  if (action.type === 'SET_PIZZA_LIST') {
+// reducer to get pizzas from database
+const pizzaList = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PIZZA_LIST':
       return action.payload;
+    default:
+      return state;
   }
-  return state;
+}
+
+// reducer to track pizzas that are being purchased and price
+const checkout = (state={totalPrice: 0, checkoutList:[]}, action) => {
+  switch (action.type) {
+    case 'ADD_PIZZA':
+      let totalPrice=state.totalPrice;
+      return { totalPrice:totalPrice += (action.payload.price), checkoutList:[...state.checkoutList, action.payload] };
+    case 'REMOVE_PIZZA':
+      let newCheckout = state.checkoutList.filter(
+        stat => stat.id != action.payload.id
+      );
+      return {totalPrice: state.totalPrice - action.payload.price, checkoutList:newCheckout};
+    default:
+      return state;
+  }
+}
+
+// reducer to keep customer information
+const customerData = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_CUSTOMER':
+      return [action.payload];
+    default:
+      return state;
+  }
+} 
+
+// reducer to add pizza in /pizzaItem
+const addPizza = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_PIZZA':
+      return [...state, {id: action.payload.id, quantity: 1}]
+    case 'REMOVE_PIZZA':
+      let newList = state.filter(
+        stat => stat.id != action.payload.id
+      );
+      return newList;
+    default:
+      return state;
+  }
 }
 
 const storeInstance = createStore(
   combineReducers({
-    pizzaReducer,
+    pizzaList,
+    checkout,
+    customerData,
+    addPizza,
   }),
   applyMiddleware(logger),
 )
