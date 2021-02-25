c77303de98403fa90e32adde2ea271935f65d188
Alvin Wong <wong.alvin13@gmail.com>
2021-02-25 14:51:10 -0600

    index updated with logger and store, added reducer

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+
+const pizzaReducer = (state = [], action) => {
+  if (action.type === '') {
+      return action.payload;
+  }
+}
+
+const storeInstance = createStore(
+  combineReducers({
+    pizzaReducer,
+  }),
+  applyMiddleware(logger),
+)
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />, 
+  </Provider>
+  document.getElementById('root'));
