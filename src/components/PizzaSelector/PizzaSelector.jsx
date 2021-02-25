1a653763cf5bd6adf3599a67d2415ebd93f69c9c
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-25 16:43:17 -0600

    pizza list is showing up

--- src/components/PizzaSelector/PizzaSelector.jsx
+++ src/components/PizzaSelector/PizzaSelector.jsx
@@ -1,6 +1,11 @@
-import PizzaItem from '../PizzaItem/PizzaItem'
+import PizzaItem from '../PizzaItem/PizzaItem';
+import {useSelector} from 'react-redux';
 
-function PizzaSelector({pizzaList}) {
+
+
+function PizzaSelector() {
+
+  const pizzaList = useSelector(store => store.pizzaList);
 
   return(
     <ul>
