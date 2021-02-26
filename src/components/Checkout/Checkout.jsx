1c55492e2f8cba21c2ce4c90ddda070dc07c5f3d
Alvin Wong <wong.alvin13@gmail.com>
2021-02-26 10:07:26 -0600

    checkout and checkout item. emptying the checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,5 +1,68 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import CheckoutItem from '../CheckoutItem/CheckoutItem'
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  // pull items from pizzaItem
+  const pizzas = useSelector(store => store.checkout);
+  const userInfo = useSelector(store => store.customerData)
+  const checkoutTotal = (checkout) => {
+    let sum = 0;
+    for (const pizza of checkout) {
+      sum += Number(pizza.price);
+    }
+    return sum.toFixed(2);
+  }
+  const emptyCheckout = () => {
+    dispatch({
+      type: 'CLEAR_CART'
+    })
+    history.push('/')
+  }
+
+  return (
+    <>
+      <h2>Step 3: Checkout</h2>
+        <div>
+          <div id="customerInfo">
+            <customerInfo>
+              <p><span>{userInfo.name}</span></p>
+              <p><span>{userInfo.address}</span></p>
+              <p>{userInfo.city}{userInfo.zip}</p>
+            </customerInfo>
+          </div>
+          <div>
+            <span id="orderType">For Delivery</span>
+          </div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Cost
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {pizzas.map(pizza => {
+                return(
+                  <CheckoutItem key={pizza.id} pizza={pizza} />
+                )
+              })}
+          </tbody>
+        </table>
+        <div>
+          <span id="checkoutTotal">Total: ${checkoutTotal(pizzas)}</span>
+        </div>
+        {/* <button onClick{() => emptyCheckout()}>Checkout</button> */}
+    </>
+  )
 }
 
 export default Checkout;
