1bdc1bacee95dad2a45ed37d1d60ea2170e70c12
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-26 11:49:49 -0600

    total is functional

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -6,11 +6,13 @@ function PizzaItem({pizza}) {
   const dispatch = useDispatch();
 
   const placeOrder = function (e) {
-    console.log('place order', e.target.value);
+    console.log('place order', e.target.id);
+    console.log('price', e.target.value);
     
     dispatch({
       type: 'ADD_PIZZA',
-      payload: e.target.value
+      payload: {id: e.target.id,
+      price: e.target.value}
     });
 
   } // end PlaceOrder
@@ -27,7 +29,7 @@ function PizzaItem({pizza}) {
         {pizza.price}
       </p> 
 
-      <button value={pizza.id} onClick={placeOrder}> Order Me!</button>
+      <button value={pizza.price} id={pizza.id} onClick={placeOrder}> Order Me!</button>
 
     </div>
   )
