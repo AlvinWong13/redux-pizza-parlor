42ddc53e72fb3c8596b1e4d1509d9c8eb241f5ce
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-26 11:07:26 -0600

    order button works

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -3,24 +3,14 @@ import {useState, useSelector} from 'react';
 
 function PizzaItem({pizza}) {
 
-  const pizzaList = useSelector(store => store.pizzaList);
-
   const dispatch = useDispatch();
 
   const placeOrder = function (e) {
-
     console.log('place order', e.target.value);
     
-    let orderedPizza;
-
-    pizzaList.forEach(pizza => {
-      console.log(pizzaList);
-      if (pizza.id === e.target.value) pizza = orderedPizza;
-    });
-
     dispatch({
       type: 'ADD_PIZZA',
-      payload: orderedPizza 
+      payload: e.target.value
     });
 
   } // end PlaceOrder
